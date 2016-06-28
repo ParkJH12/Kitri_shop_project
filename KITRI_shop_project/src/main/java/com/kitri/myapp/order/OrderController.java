2ea4b2892b82d3c8ea6abfeedabd25e130a09fc0
haibe <minhyuk118@gmail.com>
2016-06-28 14:15:19 +0900

    오터컨트롤러 -  오더리스트 오더에드 기능추가

--- KITRI_shop_project/src/main/java/com/kitri/myapp/order/OrderController.java
+++ KITRI_shop_project/src/main/java/com/kitri/myapp/order/OrderController.java
@@ -3,11 +3,16 @@ package com.kitri.myapp.order;
 import java.util.ArrayList;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.kitri.myapp.member.Join;
+import com.kitri.myapp.sell.SellBoard;
 
 @Controller
 public class OrderController {
@@ -19,11 +24,20 @@ public class OrderController {
 	}
 	
 	@RequestMapping(value = "/order/order_list.do")
-	public ModelAndView orderList(@RequestParam(value="m_num")int m_num){
-		ModelAndView mav = new ModelAndView("order/orderList");
-		ArrayList<Order> list = (ArrayList<Order>)service.getOrderList();
-		mav.addObject("list", list);
-		return mav;
+	public ModelAndView orderList(
+		@RequestParam(value="m_num")
+		int m_num,HttpServletRequest req,
+		@RequestParam(value="pb_num")int pb_num){
+			HttpSession session = req.getSession();
+			String id = (String) session.getAttribute("name");
+			Join j = service.getJoin(id);
+			SellBoard s = service.getSellBoardBynum(pb_num);
+			ArrayList<Order> o = (ArrayList<Order>)service.getOrderList();
+			ModelAndView mav = new ModelAndView("order/order_add");
+			mav.addObject("s", s); // sellboard
+			mav.addObject("j", j); // join
+			mav.addObject("o", o);
+			return mav;
 	} // m_num을 parameter로 받아(해당되는 id)의 list를 뿌려주는 기능
 	
 	@RequestMapping(value = "/order/dellist.do")
@@ -35,7 +49,16 @@ public class OrderController {
 	@RequestMapping(value = "/order/addlist.do")
 	public String addList(Order o){
 		service.InsertOrder(o);
-		return "redirect:/order/list.do";
+		return "redirect:/order/order_list.do";
 	}
 	
 }
+
+/*HttpSession session = req.getSession();
+String id = (String) session.getAttribute("name");
+Join j = service.getJoin(id);
+SellBoard s = service.getSellBoardBynum(pb_num);
+ModelAndView mav = new ModelAndView("sell/selldetail");
+mav.addObject("s", s); // board
+mav.addObject("j", j); // join
+return mav;*/
